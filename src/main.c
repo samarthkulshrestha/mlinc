38186308d2628c295aa3b9eb292baa1ddee73e6b
Samarth Kulshrestha <samarthkulshrestha@protonmail.com>
2026-01-03 15:59:46 +0530

    feat: implement basic matrix funcs

--- src/main.c
+++ src/main.c
@@ -37,3 +37,88 @@ int main(void) {
 
   return 0;
 }
+
+matrix* mat_create(mem_arena* arena, u32 rows, u32 cols) {
+  matrix* mat = PUSH_STRUCT(arena, matrix);
+
+  mat->rows = rows;
+  mat->cols = cols;
+  mat->data = PUSH_ARRAY(arena, f32, (u64)rows * cols);
+
+  return mat;
+}
+
+b32 mat_copy(matrix* dst, matrix* src) {
+  if (dst->rows != src->rows || dst->cols != src->cols) {
+    return false;
+  }
+
+  memcpy(dst->data, src->data, sizeof(f32) * (u64)dst->rows * dst->cols);
+
+  return true;
+}
+
+
+void mat_clear(matrix* mat) {
+  memset(mat->data, 0, sizeof(f32) * (u64)mat->rows * mat->cols);
+}
+
+void mat_fill(matrix* mat, f32 x) {
+  u64 size = (u64)mat->rows * mat->cols;
+
+  for (u64 i = 0; i < size; i++) {
+    mat->data[i] = x;
+  }
+}
+
+void mat_scale(matrix* mat, f32 scale) {
+  u64 size = (u64)mat->rows * mat->cols;
+
+  for (u64 i = 0; i < size; i++) {
+    mat->data[i] *= scale;
+  }
+}
+
+f32 mat_sum(matrix* mat) {
+  u64 size = (u64)mat->rows * mat->cols;
+
+  f32 sum = 0.0f;
+  for (u64 i = 0; i < size; i++) {
+    sum += mat->data[i];
+  }
+
+  return sum;
+}
+
+b32 mat_add(matrix* out, const matrix* a, const matrix* b) {
+  if (a->rows != b->rows || a->cols != b->cols) {
+    return false;
+  }
+  if (a->rows != out->rows || a->cols != out->cols) {
+    return false;
+  }
+
+  u64 size = (u64)out->rows * out->cols;
+  for (u64 i = 0; i < size; i++) {
+    out->data[i] = a->data[i] + b->data[i];
+  }
+
+  return true;
+}
+
+b32 mat_sub(matrix* out, const matrix* a, const matrix* b) {
+  if (a->rows != b->rows || a->cols != b->cols) {
+    return false;
+  }
+  if (a->rows != out->rows || a->cols != out->cols) {
+    return false;
+  }
+
+  u64 size = (u64)out->rows * out->cols;
+  for (u64 i = 0; i < size; i++) {
+    out->data[i] = a->data[i] - b->data[i];
+  }
+
+  return true;
+}
+
